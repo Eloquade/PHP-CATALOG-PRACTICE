07713c473dd5667faa2039839f1c8c5e35493c91
lawrence <perez.lawrence08@gmail.com>
2021-06-06 23:55:58 +0800

    sql

--- tblproduct.sql
+++ tblproduct.sql
@@ -1,3 +1,28 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 06, 2021 at 05:54 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blog_samples`
+--
+
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `tblproduct`
 --
@@ -15,10 +40,17 @@ CREATE TABLE `tblproduct` (
 --
 
 INSERT INTO `tblproduct` (`id`, `name`, `code`, `image`, `price`) VALUES
-(1, 'FinePix Pro2 3D Camera', '3DcAM01', 'product-images/camera.jpg', 1500.00),
-(2, 'EXP Portable Hard Drive', 'USB02', 'product-images/external-hard-drive.jpg', 800.00),
-(3, 'Luxury Ultra thin Wrist Watch', 'wristWear03', 'product-images/watch.jpg', 300.00),
-(4, 'XP 1155 Intel Core Laptop', 'LPN45', 'product-images/laptop.jpg', 800.00);
+(1, '101:AA batteries (pack of 2)', 'Batteries', 'product-images/batteries.jpg', 2.99),
+(2, '102:AA batteries (pack of 4)', 'Batteries01', 'product-images/batteries2.jpg', 5.49),
+(3, '103:Backpack (Black)', 'Bag', 'product-images/backpack.jpg', 69.99),
+(4, '104:Money belt with 6 Compartments (Black)', 'Belt', 'product-images/money.jpg', 13.49),
+(5, '105:Haversack (red)', 'Haver', 'product-images/haversack.jpg', 199.99),
+(6, '106:Swiss Army Knife (6 blades including can opener and scissors)', 'Knife', 'product-images/knife.jpg', 24.99),
+(7, '107:Duffel bag (Steel gray)', 'Bag01', 'product-images/dufflebag.jpeg', 28.50);
+
+--
+-- Indexes for dumped tables
+--
 
 --
 -- Indexes for table `tblproduct`
@@ -35,5 +67,9 @@ ALTER TABLE `tblproduct`
 -- AUTO_INCREMENT for table `tblproduct`
 --
 ALTER TABLE `tblproduct`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
