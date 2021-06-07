083fc65de9c3f658f53becc5fe25551b01a56e14
Jheave <jimenezjheave123@gmail.com>
2021-06-07 21:38:29 +0800

    fix bug

--- catalog.php
+++ catalog.php
@@ -78,7 +78,7 @@ switch($_GET["action"]) {
 								<td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
 								<td  style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
 								<td  style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
-								<td style="text-align:center;"><a href="index.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
+								<td style="text-align:center;"><a href="catalog.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
 							</tr>
 							<?php
 							$total_quantity += $item["quantity"];
@@ -111,7 +111,7 @@ switch($_GET["action"]) {
 					foreach($product_array as $key=>$value){
 				?>
 					<div class="product-item">
-						<form method="post" action="index.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>">
+						<form method="post" action="catalog.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>">
 							<div class="product-image"><img src="<?php echo $product_array[$key]["image"]; ?>"></div>
 							<div class="product-tile-footer">
 							<div class="product-title"><?php echo $product_array[$key]["name"]; ?></div>
